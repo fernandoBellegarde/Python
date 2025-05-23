7c26ef9505076901697172c5126e0befc32af935
fernandoBellegarde <bellegardefernando@gmail.com>
2025-05-22 21:55:53 -0300

    tabuada

---
+++ FIAP/aulas/capitulo005/taboada.py
@@ -0,0 +1,13 @@
+"""
+Faça a sua função imprimir todas as multiplicações entre números de 1 a 6.
+Algo como
+"""
+
+def tabuada(n):
+    for i in range(1, 11):
+        print(f"{n} x {i} = {n * i}")
+
+tabuada(1)
+tabuada(2)
+tabuada(3)
+tabuada(4)
